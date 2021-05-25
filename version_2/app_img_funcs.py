5e36aac2890eacb65f518df073aa3f67e43ac667
André Dias <andremssdias@hotmail.com>
2021-05-26 00:08:45 +0200

    Updated funcs to move blocks

--- version_2/app_img_funcs.py
+++ version_2/app_img_funcs.py
@@ -3,7 +3,6 @@ from pygame import Rect, image, transform
 
 block_total_height = 500
 
-
 def create_img_surface(path: str):
     """
     Create a Surface of the image
@@ -11,7 +10,7 @@ def create_img_surface(path: str):
     return image.load(path)
 
 
-def resize_block(block):
+def resize_block(block) -> list:
     """
         Resize/recreate buildind blocks to avoid (width, height)
         Space where bird can pass is 150
@@ -25,24 +24,3 @@ def resize_block(block):
 
     return [block_1_top, block_1_bottom]
 
-
-def display_img(screen, block, width, height):
-    """
-    Display images
-    """
-    screen.blit(block, (width, height))
-
-
-def place_block(block_top, block_bottom, screen_width: int):
-    """
-    Place building blocks at the beggining of screen width.
-    """
-    # block_total_height = 500
-    block_top.x = screen_width
-    block_bottom.x = screen_width
-
-    block_1_top_height = random.randrange(50, 300)
-    block_1_bottom_height = block_total_height - (block_1_top_height+100)
-    block_top.height = block_1_top_height
-    block_bottom.top = block_1_top_height + 100
-    block_bottom.height = block_1_bottom_height
